8e108e95248dc58ab0235462d7706057561ad31a
Miguel Cruz <Miguelc4600@gmail.com>
2018-10-22 18:17:16 -0400

    Mod loader completed

--- JS/compLoader.js
+++ JS/compLoader.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
 
-  $('#fontMod').load("htmlComponents/dependencyLoader.html");
+  $('#fontMod').load("htmlComponents/fontMod.html");
+  $('#master').load("htmlComponents/dependencyLoader.html");
   $('#navBar').load("htmlComponents/navBar.html");
   $('#footer').load("htmlComponents/footer.html");
-  $('#fontMod').load("htmlComponents/fontMod.html");
+
 
 
 });
