986a5ac6e3129f4b066154a76f64e99a6098093b
Miguel Cruz <Miguelc4600@gmail.com>
2018-12-05 03:14:39 -0500

    Added Progressive Image loading, Preloader, Fixed Pageloading Order, and added new page

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,15 @@
 new TypeIt('.type-it', {
  
 });
+
+progressively.init({
+    delay: 100,
+    throttle: 300,
+    smBreakpoint: 600,
+    onLoad: function(elem) {
+      console.log(elem);
+    },
+    onLoadComplete: function() {
+      console.log('All images have finished loading (Index) ');
+    }
+  });
--- JS/projects.js
+++ JS/projects.js
@@ -5,3 +5,16 @@ new TypeIt('.Proj-Title', {
   new TypeIt('.Proj-Subtitle', {
     
   });
+
+
+  progressively.init({
+    delay: 50,
+    throttle: 300,
+    smBreakpoint: 600,
+    onLoad: function(elem) {
+      console.log(elem);
+    },
+    onLoadComplete: function() {
+      console.log('All images have finished loading (Projects) !');
+    }
+  });
