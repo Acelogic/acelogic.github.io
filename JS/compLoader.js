ec3db806125ec04c5e8586ca72a4db45b31adde6
Miguel Cruz <Miguelc4600@gmail.com>
2018-12-05 19:15:09 -0500

    Code Cleanup

--- JS/compLoader.js
+++ JS/compLoader.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
 
-  $('#master').load("htmlComponents/dependencyLoader.html");
-  $('#fontMod').load("htmlComponents/fontMod.html");
   $('#navBar').load("htmlComponents/navBar.html");
   $('#footer').load("htmlComponents/footer.html");
 
